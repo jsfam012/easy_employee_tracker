7fa6dda6a81d86283a8dba8f1c5c678688cad9b1
Sean Ahn <edotakeout@gmail.com>
2024-01-29 20:24:35 -0500

    initial save

---
+++ index.js
@@ -0,0 +1,106 @@
+const inquirer = require('inquirer');
+const mysql = require("mysql2");
+require("console.table");
+
+const db = mysql.createConnection({
+    host: "127.0.0.1",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "employee_tracker"
+})
+
+db.connect(function(err) {
+    if(err) {
+        console.log(err);
+        return;
+    }
+})
+
+inquirer.prompt([
+    {
+        type: 'list',
+        name: 'action',
+        message: 'What would you like to do?',
+        choices: [
+            'View All Employees',
+            'Add Employee',
+            'Update Employee Role',
+            'View All Roles',
+            'Add Role',
+            'View All Departments',
+            'Add Department'
+        ] 
+    },
+])
+.then ((answers) => {
+
+
+
+    switch(answers.action) {
+        case "View All Employees":
+            // function
+            db.query("SELECT * FROM employee;", function(err, result){
+                if(err) {
+                    console.log(err);
+                    return;
+                }
+                console.table(result)
+            })
+
+            break;
+        case "View All Departments":
+             // function
+            db.query("SELECT * FROM department;", function(err, result){
+                if(err) {
+                    console.log(err);
+                    return;
+                }
+                console.table(result)
+            })
+
+            break;
+        case "View All Roles":
+            // function
+            db.query("SELECT * FROM role;", function(err, result){
+                if(err) {
+                    console.log(err);
+                    return;
+                }
+                console.table(result)
+            })
+
+            break;
+        case "Add Employee":
+            // function
+
+            break;
+        case "Update Employee Role":
+            //function
+            break;
+        case "Add Role":
+            // function
+            break;
+        case "Add Department":
+            // function
+            //  ask question first using inquirer prompt
+            // 
+            
+            // db.query(`INSERT INTO department (id, name) VALUES 
+            // (1, "Sales")`, function(err, result){
+            //     if(err) {
+            //         console.log(err);
+            //         return;
+            //     }
+            //     console.table(result)
+            // })
+
+
+            break;
+
+        default:
+            console.log("None")
+    }
+
+
+})
---
+++ seed.sql
@@ -0,0 +1,7 @@
+USE employee_tracker;
+
+INSERT INTO department (id, name) VALUES 
+	(1, "Sales"),
+    (2, "Engineering"),
+	(3, "Finance"),
+    (4, "Legal");
